a82818f62a168a6abf8fb1c56003081df0d4df59
Modi <modi@cloudtrons.com>
2019-03-21 18:51:46 +0800

    @新增歌词解析、显示

---
+++ iMusicPlayer/Module/Discover(发现)/SongDetail(歌曲详情)/Playing(播放中)/LrcView(歌词显示)/MPLrcView.swift
@@ -0,0 +1,24 @@
+//
+//  MPLrcView.swift
+//  iMusicPlayer
+//
+//  Created by Modi on 2019/3/21.
+//  Copyright © 2019 Modi. All rights reserved.
+//
+
+import UIKit
+
+class MPLrcView: BaseView {
+
+    @IBOutlet weak var lrcView: UIView! {
+        didSet {
+            
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
